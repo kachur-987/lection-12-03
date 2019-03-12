d05f61f6d2bbc157dba1dc7bd56f6c16df55b907
mdrobnych <mdrobnych@gmail.com>
2019-03-12 10:51:37 +0200

    with avatar

---
+++ app/controllers/profile_controller.rb
@@ -0,0 +1,26 @@
+class ProfileController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+  end
+  
+  def new
+    @profile = Profile.new
+  end
+  
+  def create
+    @profile = current_user.build_profile(profile_params)
+    if @profile.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+    
+  end
+  
+  private
+  
+  def profile_params
+    params.require(:profile).permit(:user_id, :nickname, :country, :avatar)
+  end
+end
