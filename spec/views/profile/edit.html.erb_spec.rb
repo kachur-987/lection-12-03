9c6ce5a2d1156eb114cc12ec8e701186f4cb921f
mdrobnych <mdrobnych@gmail.com>
2019-05-21 10:10:57 +0300

    new tests

---
+++ spec/views/profile/edit.html.erb_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "profile/edit", type: :view do
+   let!(:user) { create(:user) }
+  
+  it "should rendered partial:'profile/form'" do
+    allow(view).to receive(:current_user).and_return(user)
+    stub_template "profile/_form.html.erb" => "Create new Profile"
+    render
+    expect(rendered).to match "Create new Profile"
+  end
+  
+  before do
+   @profile = assign(:profile, Profile.create!(nickname: "NewTester", country: "New Country", user: user))
+  end
+  
+  it "should show form for 'Create new Profile'" do 
+    allow(view).to receive(:current_user).and_return(user)
+    render
+    expect(rendered).to include 'multipart/form-data'
+  end
+  
+  it "should show data from Profile" do
+    allow(view).to receive(:current_user).and_return(user)
+    pp render
+    expect(rendered).to include "NewTester"
+  end
+end
